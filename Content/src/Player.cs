b5672653f9c7214647f5c873a30d86fcddd54245
JoeFrumenti <joefrumenti@gmail.com>
2024-12-05 16:18:22 -0800

    Put player code in player object

--- Content/src/Player.cs
+++ Content/src/Player.cs
@@ -1,12 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 
-namespace WhatAmI.Content.src
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace WhatAmI
 {
-    internal class Player
+    public class Player
     {
+        float speed;
+        Vector2 pos;
+        Texture2D texture;
+        internal Player(Texture2D tex)
+        {
+            speed = 100f;
+            pos = new Vector2(0,0);
+            texture = tex;
+        }
+        internal Texture2D getTex()
+        { return texture; }
+        internal Vector2 getPos() {  return pos; }
+
+        internal void runControls(GameTime gameTime, GraphicsDeviceManager _graphics)
+        {
+
+            // The time since Update was called last.
+            float updatedSpeed = speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                pos.Y -= updatedSpeed;
+            }
+
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                pos.Y += updatedSpeed;
+            }
+
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                pos.X -= updatedSpeed;
+            }
+
+            if (kstate.IsKeyDown(Keys.Right))
+            {
+                pos.X += updatedSpeed;
+            }
+
+
+            if (pos.X > _graphics.PreferredBackBufferWidth - texture.Width / 2)
+            {
+                pos.X = _graphics.PreferredBackBufferWidth - texture.Width / 2;
+            }
+            else if (pos.X < texture.Width / 2)
+            {
+                pos.X = texture.Width / 2;
+            }
+
+            if (pos.Y > _graphics.PreferredBackBufferHeight - texture.Height / 2)
+            {
+                pos.Y = _graphics.PreferredBackBufferHeight - texture.Height / 2;
+            }
+            else if (pos.Y < texture.Height / 2)
+            {
+                pos.Y = texture.Height / 2;
+            }
+        }
     }
 }
--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+
 namespace WhatAmI
 {
     public class Game1 : Game
@@ -9,6 +10,8 @@ namespace WhatAmI
         Texture2D ballTexture;
         Vector2 ballPosition;
         float ballSpeed;
+
+        Player player;
 
 
         private GraphicsDeviceManager _graphics;
@@ -32,15 +35,17 @@ namespace WhatAmI
             ballPosition = new Vector2(_graphics.PreferredBackBufferWidth  / 2,
                                        _graphics.PreferredBackBufferHeight / 2);
             ballSpeed = 100f;
+            
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            
             // TODO: use this.Content to load your game content here
             ballTexture = Content.Load<Texture2D>("ball");
+            player = new Player(ballTexture);
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,54 +54,11 @@ namespace WhatAmI
                 Exit();
 
             // TODO: Add your update logic here
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-                            Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
 
             // The time since Update was called last.
             float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            var kstate = Keyboard.GetState();
-
-            if (kstate.IsKeyDown(Keys.Up))
-            {
-                ballPosition.Y -= updatedBallSpeed;
-            }
-
-            if (kstate.IsKeyDown(Keys.Down))
-            {
-                ballPosition.Y += updatedBallSpeed;
-            }
-
-            if (kstate.IsKeyDown(Keys.Left))
-            {
-                ballPosition.X -= updatedBallSpeed;
-            }
-
-            if (kstate.IsKeyDown(Keys.Right))
-            {
-                ballPosition.X += updatedBallSpeed;
-            }
-
-
-            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
-            {
-                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
-            }
-            else if (ballPosition.X < ballTexture.Width / 2)
-            {
-                ballPosition.X = ballTexture.Width / 2;
-            }
-
-            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
-            {
-                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
-            }
-            else if (ballPosition.Y < ballTexture.Height / 2)
-            {
-                ballPosition.Y = ballTexture.Height / 2;
-            }
+            player.runControls(gameTime, _graphics);
 
 
             base.Update(gameTime);
@@ -108,7 +70,7 @@ namespace WhatAmI
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(ballTexture, ballPosition, null, Color.White, 0f, new Vector2(ballTexture.Width/2,ballTexture.Height/2),Vector2.One,SpriteEffects.None,0f);
+            _spriteBatch.Draw(player.getTex(), player.getPos(), null, Color.White, 0f, new Vector2(ballTexture.Width/2,ballTexture.Height/2),Vector2.One,SpriteEffects.None,0f);
             _spriteBatch.End();
 
 
